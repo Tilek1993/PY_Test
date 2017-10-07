ede1828c7dc7e7ccee675ea450b829a49ea235d9
Tilek1993 <tilekshyikyn.93@gmail.com>
2017-10-07 23:36:22 +0600

    PY_Test

---
+++ Python8.py
@@ -0,0 +1,34 @@
+
+cities = ['New York','Moscow','New Dehli','Simferopol','Toronto']
+print(cities)
+print(len(cities))
+
+print(cities[0])
+print(cities[4])
+print(cities[-1])
+print(cities[-2])
+print(cities[2].upper())
+
+cities[2] = 'Tula'
+print(cities)
+
+cities.append('Kursk')
+cities.append('Yalta')
+print(cities)
+
+cities.insert(2, 'Feodosiya')
+print(cities)
+
+
+del cities[-1]
+print(cities)
+
+cities.remove('Tula')
+print(cities)
+
+deleted_city = cities.pop()
+print('Deleted city is:' + deleted_city)
+print(cities)
+
+cities.sort()
+print(cities)
